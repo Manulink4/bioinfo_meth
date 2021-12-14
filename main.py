ec18df967dc033d95b787decdeeb2e3cb520f36b
saizk <sergioaizcorbe@hotmail.com>
2021-12-14 21:23:35 +0100

    Added NZM9 column to dfs and some plots

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import pandas as pd
 
-df = pd.read_csv("meth_counts/GSE153594_Methylation_Counts.csv", sep=",", decimal=".", dtype="object")
-# print(df)
 
-df = df[["Chr", "Start", "End", "NZM9"]]
-
-
+def main():
+    df = pd.read_csv("meth_counts/GSE153594_Methylation_Counts.csv", sep=",", decimal=".", dtype="object")
+    df = df[["Chr", "Start", "End", "NZM9"]]
 
 
-
-
+if __name__ == '__main__':
+    main()
