3e6a6610abe7bcd78409d644fa88ae20240eab7e
Manulink4 <manung2000@gmail.com>
2021-12-24 01:49:30 +0100

    test pa mi primo

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 def main():
     df = pd.read_csv("meth_counts/GSE153594_Methylation_Counts.csv", sep=",", decimal=".", dtype="object")
     df = df[["Chr", "Start", "End", "NZM9"]]
+    print("Hello")
 
 
 if __name__ == '__main__':
